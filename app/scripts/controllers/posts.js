36d884a6126f56adbfbe50f547b69da0b2b3b72e
ed <ejbyne@gmail.com>
2015-01-13 18:27:49 +0000

    controller linked to firebase database

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,19 +1,17 @@
 'use strict';
 
 app.controller('PostsCtrl', function ($scope, Post) {
-  $scope.posts = [];
+  $scope.posts = Post.all;
+
   $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function () {
-    Post.save($scope.post, function (ref) {
-      $scope.posts[ref.name] = $scope.post;
+    Post.create($scope.post).then(function () {
       $scope.post = {url: 'http://', title: ''};
     });
   };
 
-  $scope.deletePost = function (index) {
-    Post.delete({id: PostId}, function () {
-      delete $scope.posts[postId];
-    });
+  $scope.deletePost = function (post) {
+    Post.delete(post);
   };
 });
