5ca80d164460907c91d5495ba7148d0ff51a2a06
ed <ejbyne@gmail.com>
2015-01-13 16:16:46 +0000

    set up posts controller and posts view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* global app:true */
+/* exported app */
 /**
  * @ngdoc overview
  * @name angNewsApp
@@ -8,7 +10,7 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('angNewsApp', [
     'ngAnimate',
     'ngCookies',
@@ -20,14 +22,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
       });
   });
